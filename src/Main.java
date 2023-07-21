f0be886e78f83684a3eae94dff7274bd00674451
Vel <korzhavin.vel@yandex.ru>
2023-07-21 15:02:20 +0700

    Method task1,2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
-    public static void determineTheYear (int year) {
+    public static void determineTheYear(int year) {
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println(year + " год является високосным.");
         } else {
             System.out.println(year + " год не является високосным.");
         }
 
+    }
+
+    public static void determineTypeOS(int year1, int typeOS) {
+        if (year1 > LocalDate.now().getYear() || typeOS != 0 && typeOS != 1) {
+            System.out.println("Ваша ОС не поддерживается");
+        } else {
+            System.out.println("Установите " + (year1 > 2015 ? "полную " : "облегченную ") + "версию приложения для "
+                    + (typeOS == 0 ? "iOS" : "Android") + " по ссылке");
+        }
     }
 
     public static void main(String[] args) {
@@ -23,10 +33,17 @@ public class Main {
         int year = scanner.nextInt();
         determineTheYear(year);
 
+
     }
 
     public static void task2() {
         System.out.println("Задача 2");
+        Scanner scanner1 = new Scanner(System.in);
+        int currentYear = LocalDate.now().getYear();
+        System.out.println(currentYear);
+        System.out.println("Введите тип ОС, где 0 - iOS, 1 - Android - ");
+        int typeOS = scanner1.nextInt();
+        determineTypeOS(currentYear, typeOS);
     }
 
     public static void task3() {
